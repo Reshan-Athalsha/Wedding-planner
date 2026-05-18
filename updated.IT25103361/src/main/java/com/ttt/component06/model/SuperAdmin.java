caa28bc2257e9b7eafc00d257d370ca6bf01db90
IT25103361 <IT25103361@my.sliit.lk>
2026-05-18 15:26:57 +0530

    SuperAdmin.java

--- updated.IT25103361/src/main/java/com/ttt/component06/model/SuperAdmin.java
+++ updated.IT25103361/src/main/java/com/ttt/component06/model/SuperAdmin.java
@@ -1,8 +1,18 @@
 package com.ttt.component06.model;
-/** COMPONENT 06 - SuperAdmin | OOP: INHERITANCE, POLYMORPHISM */
+
 public class SuperAdmin extends Admin {
-    public SuperAdmin() {}
-    public SuperAdmin(String adminId, String username, String password, String email) { super(adminId, username, password, email); }
-    @Override public String getRoleName() { return "SUPER_ADMIN"; }
-    @Override public String getPermissions() { return "Full System Access, Add/Remove Admins"; }
+    
+    public SuperAdmin(String adminId, String username, String password, String email) {
+        super(adminId, username, password, email); 
+    }
+    
+    @Override 
+    public String getRoleName() {
+        return "SUPER_ADMIN";
+    }
+    
+    @Override 
+    public String getPermissions() {
+        return "Full System Access, Add/Remove Admins"; 
+    }
 }
